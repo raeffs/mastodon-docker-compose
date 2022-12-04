6ad1508ae933eb6cee707002952fae0f9db9f5eb
Raphael Fleischlin <git@raeffs.dev>
2022-12-04 14:09:02 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Mastodon Setup with Docker Compose
+
+This repository contains the configuration files I described in my blog posts about setting up an own Mastodon instance with Docker:
+
+- [Part 1](https://www.raeffs.dev/blog/self-hosting/2022-11-11-mastodon-with-docker/)
+- [Part 2](https://www.raeffs.dev/blog/self-hosting/2022-12-04-mastodon-improvements/)
---
+++ provision.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Migrating database..."
+bundle exec rake db:migrate
+
+CHECK=/mastodon/public/system/provisioned
+
+if [ -f "$CHECK" ]; then
+    echo "Provisioning not required"
+else
+    echo "Provisioning mastodon..."
+
+    bin/tootctl accounts create $MASTODON_ADMIN_USERNAME --email $MASTODON_ADMIN_EMAIL --confirmed --role Admin
+
+    echo "Provisioning done"
+    touch "$CHECK"
+fi
+
