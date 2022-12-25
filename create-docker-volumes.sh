4856bb263187db1e73a810be17ebda73da034f95
Raphael Fleischlin <git@raeffs.dev>
2022-12-25 11:45:24 +0100

    add redis volume

--- create-docker-volumes.sh
+++ create-docker-volumes.sh
@@ -1,6 +1,7 @@
 
 docker volume create mastodon-db-volume
 docker volume create mastodon-es-volume
+docker volume create mastodon-redis-volume
 
 docker volume create caddy-data-volume
 docker volume create caddy-config-volume
